c8b175fbefe27102a19293008b3b494c041d0b12
Mikele91 <michele.amoruso91.8@gmail.com>
2021-09-28 16:55:37 +0200

    Esercizio terminato

---
+++ src/components/Title.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="quadrato">
+    <h1>{{msg}}</h1>
+    <div id="hero"></div>
+  </div>
+</template>
+
+<script>
+    export default {
+    name: 'Title',
+    props: {
+        msg: String
+    }
+    }
+</script>
+
+<style lang="scss">
+    #hero{
+        background-image: url("https://fantascienzaitalia.com/wp-content/uploads/2018/10/Saluto-spock.jpg");
+        width: 50rem;
+        height: 37.5rem;
+        margin: auto;
+        background-size: cover;
+    } 
+        
+
+</style>
